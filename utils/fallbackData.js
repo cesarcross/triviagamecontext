1e2823556cc26ae868319959bcab3ac6f946d7cd
Cesar <cesarcorregiari@gmail.com>
2021-02-22 22:09:46 -0300

    create utils folder and add backup data in case api fetch err

---
+++ utils/fallbackData.js
@@ -0,0 +1,87 @@
+export const fallbackData = [
+  {
+    category: 'History',
+    type: 'boolean',
+    difficulty: 'hard',
+    question: 'Japan was part of the Allied Powers during World War I.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Science: Mathematics',
+    type: 'boolean',
+    difficulty: 'hard',
+    question:
+      'The binary number 101001101 is equivalent to the Decimal number 334;',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'Science: Computers',
+    type: 'boolean',
+    difficulty: 'hard',
+    question: 'DHCP stands for Dynamic Host Configuration Port.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'General Knowledge',
+    type: 'boolean',
+    difficulty: 'hard',
+    question: 'In Scandinavian languages, the letter &Aring; means river.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Entertainment: Video Games',
+    type: 'boolean',
+    difficulty: 'hard',
+    question:
+      'All of these maps were in Tom Clancys Rainbow Six Siege&quot; on its initial release: House, Clubhouse, Border, Consulate.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'History',
+    type: 'boolean',
+    difficulty: 'hard',
+    question:
+      'During the Winter War, the amount of Soviet Union soliders that died or went missing was five times more than Finlands.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Entertainment: Music',
+    type: 'boolean',
+    difficulty: 'hard',
+    question:
+      'Pete Townshend solo album, &quot;White City: A Novel&quot;, is set in the metropolitan area of Chicago.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    category: 'Entertainment: Music',
+    type: 'boolean',
+    difficulty: 'hard',
+    question:
+      'The song Scatman World was released after Scatman (Ski-Ba-Bop-Ba-Dop-Bop).',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'History',
+    type: 'boolean',
+    difficulty: 'hard',
+    question: 'The man that shot Alexander Hamilton was named Aaron Burr.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    category: 'Geography',
+    type: 'boolean',
+    difficulty: 'hard',
+    question: 'Only one country in the world starts with the letter Q.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+];
